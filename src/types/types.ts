db0394e2601061aa15d3c85abdb9a1589a66c4c1
divyeshbsagcorps <divyeshbsagcorps@gmail.com>
2026-08-27 20:28:17 +0530

    first commit

---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+export type FormValues = {
+  name: string;
+  email: string;
+  feedback: string;
+  satisfaction: string;
+  recommend: string;
+  department: string;
+  source: string;
+  likedfeatures: string[];
+  platforms: string[];
+};
